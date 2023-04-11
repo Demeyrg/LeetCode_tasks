a026ced97d61076d58e28a58ee8d0e64bc44b552
Slava Aleinikov <slava.aleinikov@mail.ru>
2023-04-11 12:35:25 +0600

    70. Climbing Stairs

--- src/git/example/easy/ClimbingStairs.java
+++ src/git/example/easy/ClimbingStairs.java
@@ -23,19 +23,40 @@ public class ClimbingStairs {
         System.out.println(climbStairs(4));
         System.out.println(climbStairs(5));
         System.out.println(climbStairs(6));
-        System.out.println(climbStairs(45));
-        }
-
-    public static int climbStairs(int n) {
-        if (n == 1)
-            return 1;
-        if (n == 2)
-            return 2;
-        int pow = (n / 2) + 1;
-        if (n % 2 == 0) {
-            return (int) Math.pow(n, pow) + 1;
-        } else {
-            return (int) Math.pow(n, pow);
-        }
+        System.out.println(climbStairs(40));
     }
+
+    /***
+     *  0 ms, 39.2 MB
+     *
+     * @param n as int
+     * @return int
+     */
+    public static int climbStairs(int n) {
+        int previous = 1;
+        int prePrevious = 0;
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result = previous + prePrevious;
+            prePrevious = previous;
+            previous = result;
+        }
+        return result;
+    }
+
+    /*
+        It works, but it takes quite a long time.
+     */
+//    public static int climbStairs(int n) {
+//        if (n == 1)
+//            return 1;
+//        if (n == 2)
+//            return 2;
+//        if (n == 3)
+//            return 3;
+//        int result = climbStairs(n - 1) + climbStairs(n - 2);
+//        return result;
+//    }
+    
 }
+
